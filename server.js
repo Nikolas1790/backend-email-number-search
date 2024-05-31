2d4cd5fc1199dbe60a3575825dcc93a9e06ed4d8
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-05-31 23:21:52 +0300

    node-version

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
 // 9toZMwxUBwtb8YY9
 // mongodb+srv://Nik:<password>@cluster0.rj2x8xl.mongodb.net/
 
-// import mongoose from "mongoose";
-// import app from "./app.js";
-
-// const { DB_HOST, PORT } = process.env;
-
-// mongoose.connect(DB_HOST)
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log("Database connection successful");
-//       console.log(`Server runing on ${PORT} PORT`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error.message);
-//     process.exit(1);
-//   });
-
-import express from "express";
+// mongodb+srv://Nik:9toZMwxUBwtb8YY9@cluster0.rj2x8xl.mongodb.net/my-contacts?retryWrites=true&w=majority&appName=Cluster0
 
 
+import mongoose from "mongoose";
+import app from "./app.js";
 
-const app = express();
+const { DB_HOST, PORT } = process.env;
 
-app.listen(4000, () => console.log("hello world"))
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+      console.log(`Server runing on ${PORT} PORT`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+
+
+
+
+// import express from "express";
+
+
+
+// const app = express();
+
+// app.listen(4000, () => console.log("hello world"))
